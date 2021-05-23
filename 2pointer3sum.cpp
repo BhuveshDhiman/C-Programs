a8b47db8164190a5154c679a58e6f30ad12a1aef
Bhuvesh Dhiman <67685811+BhuveshDhiman@users.noreply.github.com>
2021-05-23 16:47:21 +0530

    2 Pointer 3 Sum

---
+++ 2pointer3sum.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int target;
+    cin >> target;
+    vector<int> a(n);
+
+    for (auto &i : a)
+    {
+        cin >> i;
+    }
+
+    bool found = false;
+    sort(a.begin(), a.end());
+
+    for (int i = 0; i < n; i++) // O(n^2)
+    {
+        int low = i + 1, high = n - 1;
+
+        while (low < high)
+        {
+            int curr = a[i] + a[low] + a[high];
+            if (curr == target)
+            {
+                found = true;
+            }
+            if (curr < target)
+            {
+                low++;
+            }
+            else
+            {
+                high--;
+            }
+        }
+    }
+
+    if (found)
+    {
+        cout << true;
+    }
+    else
+    {
+        cout << false;
+    }
+
+    return 0;
+}
